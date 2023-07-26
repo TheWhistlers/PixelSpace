e60b5bd7cb409830aab1b2b53e17bc0f6828e252
Whistler <87767647+TheWhistlers@users.noreply.github.com>
2023-07-26 20:27:12 +0800

    Delete Scripts directory

--- Scripts/Preferences/PathPreferences.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class PathPreferences
-{
-    public static string ITEM_TEXTURE_PATH { get; set; } = $@"{GameManager.Persistent}/games/resources/textures/items/";
-    public static string BLOCK_TEXTURE_PATH { get; set; } = $@"{GameManager.Persistent}/games/resources/textures/blocks/";
-}
